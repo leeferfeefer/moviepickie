5e3c35d3319a410544aa621077c2536bcb1bcae1
leeferfeefer <danbfjr@gmail.com>
2021-07-13 09:27:11 -0400

    Added read movies button

--- App.js
+++ App.js
@@ -5,14 +5,22 @@ import {
   StyleSheet,
   Text,
   View,
+  TouchableHighlight,
 } from 'react-native';
 
 const App = () => {
+
+    const readMoviesButtonPressed = () => {
+        console.log("Button pressed...");
+    };
+
     return (
         <SafeAreaView style={styles.container}>
             <StatusBar barStyle='light-content' />
             <View style={styles.view}>
-                <Text>Hello World</Text>
+                <TouchableHighlight style={styles.readMoviesListButton} onPress={readMoviesButtonPressed}>
+                    <Text style={styles.readMoviesListButtonText}>Read Movies</Text>
+                </TouchableHighlight>
             </View>
         </SafeAreaView>
     );
@@ -27,6 +35,16 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    readMoviesListButton: {
+        backgroundColor: 'blue',
+        height: 50,
+        width: 100,
+        justifyContent: 'space-around'
+    },
+    readMoviesListButtonText: {
+        color: 'white',
+        textAlign: 'center'
     }
 });
 
