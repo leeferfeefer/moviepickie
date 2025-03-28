f660d2cc17c3e28d7107b2f3c6030d9400654a6a
Dan Fincher <danbfjr@gmail.com>
2025-03-28 10:53:47 -0400

    fixed movieList typing in newscreen

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -64,7 +64,7 @@ export const MovieList = React.memo(
             </>
         );
     },
-);
+) as <T extends object>(props: MovieListProps<T>) => React.JSX.Element;
 
 const styles = StyleSheet.create({
     emptyContainer: {
